8da6e7bdac2c5c341cb6e12668a87aef4e7da98d
kishormahmud9 <topfazil999@gmail.com>
2025-01-07 15:40:25 +0600

    glowthentic design start

--- glowthentic-frontend/src/page/home/HomePage.jsx
+++ glowthentic-frontend/src/page/home/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Container from "../../components/Container";
+import { Icon } from "@iconify/react";
 
 const HomePage = () => {
   return (
     <div>
       <Container>
         <h2>This is Home Page</h2>
+        <Icon icon="solar:airbuds-remove-outline" width="24" height="24" />
       </Container>
     </div>
   );
