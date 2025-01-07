f0ec892247e5e01091dc6ee947d8f67abe6eb722
kishormahmud9 <topfazil999@gmail.com>
2025-01-07 17:51:51 +0600

    glowthentic design start

--- glowthentic-frontend/src/components/Menu.jsx
+++ glowthentic-frontend/src/components/Menu.jsx
@@ -4,30 +4,39 @@ import { Icon } from "@iconify/react";
 
 const Menu = () => {
   return (
-    <div>
+    <div className="px-2">
       <Container>
+        <div className="py-2 flex justify-start gap-1 items-center font-semibold text-lg ps-4 cursor-pointer">
+          <Icon
+            icon="solar:alt-arrow-left-linear"
+            className="font-semibold"
+            width="24"
+            height="24"
+          />
+          Women Skincare
+        </div>
         <ul>
-          <li className="bg-white py-2 px-5 flex justify-between border font-bold cursor-pointer">
+          <li className="bg-white py-2 px-5 flex justify-between border font-semibold cursor-pointer">
             Women Wake Up{" "}
             <Icon icon="solar:alt-arrow-right-bold" width="24" height="24" />
           </li>
-          <li className="bg-white py-2 px-5 flex justify-between border font-bold cursor-pointer">
+          <li className="bg-white py-2 px-5 flex justify-between border font-semibold cursor-pointer">
             Women Wake Up{" "}
             <Icon icon="solar:alt-arrow-right-bold" width="24" height="24" />
           </li>
-          <li className="bg-white py-2 px-5 flex justify-between border font-bold cursor-pointer">
+          <li className="bg-white py-2 px-5 flex justify-between border font-semibold cursor-pointer">
             Women Wake Up{" "}
             <Icon icon="solar:alt-arrow-right-bold" width="24" height="24" />
           </li>
-          <li className="bg-white py-2 px-5 flex justify-between border font-bold cursor-pointer">
+          <li className="bg-white py-2 px-5 flex justify-between border font-semibold cursor-pointer">
             Women Wake Up{" "}
             <Icon icon="solar:alt-arrow-right-bold" width="24" height="24" />
           </li>
-          <li className="bg-white py-2 px-5 flex justify-between border font-bold cursor-pointer">
+          <li className="bg-white py-2 px-5 flex justify-between border font-semibold cursor-pointer">
             Women Wake Up{" "}
             <Icon icon="solar:alt-arrow-right-bold" width="24" height="24" />
           </li>
-          <li className="bg-white py-2 px-5 flex justify-between border font-bold cursor-pointer">
+          <li className="bg-white py-2 px-5 flex justify-between border font-semibold cursor-pointer">
             Women Wake Up{" "}
             <Icon icon="solar:alt-arrow-right-bold" width="24" height="24" />
           </li>
