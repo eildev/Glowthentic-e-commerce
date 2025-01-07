a1789580de064e70a1f5acacc23d69f87e6a50f4
kishormahmud9 <topfazil999@gmail.com>
2025-01-07 15:04:25 +0600

    glowthentic design start

--- glowthentic-frontend/src/page/home/HomePage.jsx
+++ glowthentic-frontend/src/page/home/HomePage.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Container from "../../components/Container";
 
 const HomePage = () => {
   return (
     <div>
-      <h2>This is Home Page</h2>
+      <Container>
+        <h2>This is Home Page</h2>
+      </Container>
     </div>
   );
 };
