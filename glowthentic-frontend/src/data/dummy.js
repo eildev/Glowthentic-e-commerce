0d7d866708a8c8b2c9cb19b8c82c6cbf05ae8da6
kishormahmud9 <topfazil999@gmail.com>
2025-01-08 14:26:04 +0600

    glowthentic design start

---
+++ glowthentic-frontend/src/data/dummy.js
@@ -0,0 +1,52 @@
+export const navData = [
+    {
+        id: 1,
+        title: "Women Make Up",
+        dropdown: true
+    },
+    {
+        id: 2,
+        title: "Hair",
+        dropdown: true
+    },
+    {
+        id: 3,
+        title: "Mom & Baby",
+        dropdown: true
+    },
+    {
+        id: 4,
+        title: "Fragrance",
+        dropdown: true
+    },
+    {
+        id: 5,
+        title: "Women Skincare",
+        dropdown: true
+    },
+    {
+        id: 6,
+        title: "Personal Care",
+        dropdown: true
+    },
+    {
+        id: 7,
+        title: "Buy 1 Get 1",
+        dropdown: false
+    },
+    {
+        id: 8,
+        title: "Men",
+        dropdown: false
+    },
+    {
+        id: 9,
+        title: "Undergarments",
+        dropdown: false
+    },
+    {
+        id: 10,
+        title: "Giga Sale",
+        dropdown: false
+    },
+]
