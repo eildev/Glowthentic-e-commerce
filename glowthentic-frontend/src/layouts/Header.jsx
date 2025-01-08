bc6f50eddfc713837dc19451a266de1fa9e8ea4e
NayemHassan <nayem15-12498@diu.edu.bd>
2025-01-08 10:52:29 +0600

    design

--- glowthentic-frontend/src/layouts/Header.jsx
+++ glowthentic-frontend/src/layouts/Header.jsx
@@ -1,7 +1,25 @@
 import { Icon } from "@iconify/react";
 import Container from "../components/Container";
-
+import  { useState, useEffect } from "react";
 const Header = () => {
+  const [query, setQuery] = useState(""); // Search query
+  const [suggestions, setSuggestions] = useState([]); // Suggestions list
+
+  const fetchSuggestions = async (searchText) => {
+    // Replace this with an actual API call
+    const mockData = ["Apple", "Banana", "Cherry", "Date", "Elderberry", "Fig", "Grape"];
+    const filteredData = mockData.filter((item) =>
+      item.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setSuggestions(filteredData);
+  };
+  useEffect(() => {
+    if (query) {
+      fetchSuggestions(query);
+    } else {
+      setSuggestions([]); // Clear suggestions when query is empty
+    }
+  }, [query]);
   return (
     <div  className="bg-primary">
       <Container>
@@ -40,14 +58,39 @@ const Header = () => {
   </div>
   <div className="navbar-center hidden lg:flex">
     <div className="relative">
-    <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500">
+
+    <a href="">  <span className="absolute right-3  z-10 top-1/2 transform -translate-y-1/2 text-gray-500">
     <Icon icon="ic:outline-search" className="text-black " width="24" height="24" />
-    </span>
+    </span></a>
+
     <input 
       type="text" 
       placeholder="Search for products, brands..." 
-      className="input input-bordered text-black h-9 rounded-e-3xl rounded-s-3xl md:w-96 pl-10"
+      value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      className="input z-10 input-bordered text-black h-9 rounded-e-3xl rounded-s-3xl md:w-96 pe-10"
     />
+    {/* //Search Show Text */}
+    <div >
+    {suggestions.length > 0 && (
+            <ul className="absolute left-0 right-0 text-black  bg-white shadow-md rounded-b-3xl ">
+              {suggestions.map((item, index) => (
+                <li
+                  key={index}
+                  className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+                  onClick={() => {
+                    setQuery(item); // Fill input with selected suggestion
+                    setSuggestions([]); // Clear suggestions
+                  }}
+                >
+                  {item}
+                </li>
+              ))}
+
+              {/* //Search Show Text End */}
+            </ul>
+          )}
+    </div>
 </div>
   </div>
   <div className="navbar-end">
