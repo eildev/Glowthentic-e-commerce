65a24acd6f30e2e7013b8331e72b325a6d462a96
NayemHassan <nayem15-12498@diu.edu.bd>
2025-01-09 13:12:46 +0600

    new

--- glowthentic-frontend/src/components/typography/RegularButton.jsx
+++ glowthentic-frontend/src/components/typography/RegularButton.jsx
@@ -1,12 +1,11 @@
 import cn from "../../utils/cn";
 
-const RegularButton = ({ children, className }) => {
+const RegularButton = ({ children ,className}) => {
   return (
     <button
       className={`${cn(
         "bg-secondary  p-3 md:px-14  lg:px-14 xl:px-16 px-10 rounded-md text-white md:text-lg lg:text-xl",
-        className,
-        
+        className
       )}`}
     >
       {children}
