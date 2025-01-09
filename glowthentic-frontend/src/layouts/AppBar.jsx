74ca29ee4bb275ff91b7c8504ebc37ebc3cef2d7
kishormahmud9 <topfazil999@gmail.com>
2025-01-09 14:50:36 +0600

    glowthentic design start

--- glowthentic-frontend/src/layouts/AppBar.jsx
+++ glowthentic-frontend/src/layouts/AppBar.jsx
@@ -2,6 +2,12 @@ const AppBar = () => {
   return (
     <div className="fixed bottom-0 bg-white w-full right-0">
       <h2>This is My App Bar</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae magni
+        corporis quas possimus incidunt natus, laudantium, temporibus iusto,
+        doloribus facilis molestiae sunt enim! Expedita, animi laudantium beatae
+        voluptatibus dolorum reiciendis?
+      </p>
     </div>
   );
 };
