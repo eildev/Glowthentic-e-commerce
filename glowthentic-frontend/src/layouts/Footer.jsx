ab311434c7b10f55e3529a86f9e148817fd88dfb
shahriarhossainjihad <jihadshahriar@gmail.com>
2025-01-09 11:26:34 +0600

    shahriar footer design ongoing

--- glowthentic-frontend/src/layouts/Footer.jsx
+++ glowthentic-frontend/src/layouts/Footer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Container from "../components/Container";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
 const Footer = () => {
   return (
     <div>
       <Container>
-        <footer className="footer bg-base-200 text-base-content p-10">
+        <footer className="footer bg-base-200 text-base-content p-5">
           <nav>
             <h6 className="footer-title">Services</h6>
             <a className="link link-hover">Branding</a>
@@ -20,12 +21,6 @@ const Footer = () => {
             <a className="link link-hover">Jobs</a>
             <a className="link link-hover">Press kit</a>
           </nav>
-          <nav>
-            <h6 className="footer-title">Legal</h6>
-            <a className="link link-hover">Terms of use</a>
-            <a className="link link-hover">Privacy policy</a>
-            <a className="link link-hover">Cookie policy</a>
-          </nav>
           <form>
             <h6 className="footer-title">keep in touch with Glowthentic</h6>
             <fieldset className="form-control w-80">
@@ -39,31 +34,36 @@ const Footer = () => {
                 <span className="label-text">Enter your email address</span>
               </label>
               <div className="join">
-                <input
+                {/* <input
                   type="text"
                   placeholder="Enter your email address"
-                  className="input input-bordered join-item"
-                />
-                <button className="btn btn-primary join-item">Subscribe</button>
+                  className={twMerge} input input-bordered
+                /> */}
+                <button className="btn btn-primary">Subscribe</button>
               </div>
             </fieldset>
           </form>
         </footer>
 
-        <footer className="footer bg-neutral text-neutral-content items-center p-4">
-          <aside className="grid-flow-col items-center gap-2">
-            <div className="grid-flow-col">
+        <footer className="footer grid md:grid-cols-2 lg:grid-cols-3 bg-neutral text-neutral-content items-center px-5 py-4">
+          <aside className="grid-flow-row lg:grid-flow-col items-center gap-5">
+            <div className="flex justify-between items-center gap-1">
+              <span>
+                <Icon icon="mdi-light:map-marker" width="24" height="24" />
+              </span>
 
-                <p>House 41, Block E, Road-06, Banasree, 
-                Dhaka, Dhaka, Bangladesh</p>
-                
-                
+              <p>
+                House 41, Block E, Road-06, Banasree, Dhaka, Dhaka, Bangladesh
+              </p>
+              <span className="hidden lg:block text-xl">|</span>
             </div>
-            <span>|</span>
-            <div>
-            <p>1-802-526-2463</p>
+
+            <div className="flex gap-1">
+              <Icon icon="mdi-light:phone" width="24" height="24" />
+              <p>1-802-526-2463</p>
             </div>
           </aside>
+          <div className="hidden lg:block"></div>
           <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
             <a>
               <svg
@@ -102,20 +102,9 @@ const Footer = () => {
         </footer>
       </Container>
       <Container>
-        <footer className="footer bg-base-200 text-base-content border-base-300 border-t px-10 py-4">
-        <aside className="grid-flow-col items-center">
-            <svg
-              width="36"
-              height="36"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-              fillRule="evenodd"
-              clipRule="evenodd"
-              className="fill-current"
-            >
-              <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
-            </svg>
-            <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+        <footer className="footer bg-base-200 text-base-content border-base-300 border-t px-10 py-4 px-5">
+          <aside className="grid-flow-col items-center">
+            <p>© {new Date().getFullYear()} Glowthentic - All Rights Reserved.</p>
           </aside>
           <nav className="md:place-self-center md:justify-self-end">
             <div className="grid grid-flow-col gap-4">
