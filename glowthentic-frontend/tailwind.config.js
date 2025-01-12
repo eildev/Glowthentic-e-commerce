e85c9f8d3392116a86545ade1ce41c6d0f3c133e
kishormahmud9 <topfazil999@gmail.com>
2025-01-12 10:39:25 +0600

    glowthentic design start

--- glowthentic-frontend/tailwind.config.js
+++ glowthentic-frontend/tailwind.config.js
@@ -12,11 +12,12 @@ export default {
         secondary: '#FA8232',
         dark: '#0c0c0c',
         gray: '#606060',
+        danger: '#FF342D',
         'white-gray': '#CBCBCB',
         'secondary-gradient-1': 'rgba(250, 130, 50, 0.3)', // 30% opacity
         'secondary-gradient-2': 'rgba(250, 130, 50, 0.15)', // 15% opacity
-        bodergraythin: '#AFAFAF',
-        bodergraybold: '#7A7A7A',
+        "gray-thin": '#AFAFAF',
+        "gray-bold": '#7A7A7A',
 
       },
       fontWeight: {
