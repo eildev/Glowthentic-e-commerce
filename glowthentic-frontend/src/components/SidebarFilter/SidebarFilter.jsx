e56fb1549ee9fdac78827cf02d36be650685053f
NayemHassan <nayem15-12498@diu.edu.bd>
2025-01-12 16:59:44 +0600

    filter design

--- glowthentic-frontend/src/components/SidebarFilter/SidebarFilter.jsx
+++ glowthentic-frontend/src/components/SidebarFilter/SidebarFilter.jsx
@@ -9,10 +9,11 @@ import RegularButton from "../typography/RegularButton";
 const SidebarFilter = () => {
   return (
     <div className="p-3 ">
-      <Container>
-        <div className="max-w-72 mt-5">
+      <Container >
+        <div className="max-w-72 bg-white">
+        <div className="mt-5 p-2">
           <HeadTitle>Filter</HeadTitle>
-          <div className="mt-5 bg-white">
+          <div className="mt-5 ">
             <hr className="text-gray-thin" />
             <div className="p-4">
               <HeadTitle className="text-sm md:text-lg lg:text-lg xl:text-lg">
@@ -23,7 +24,7 @@ const SidebarFilter = () => {
             <div></div>
             {/* //Here Filter tag End// */}
             <div className="p-4">
-              <Link className="text-secondary">Clear All Filters</Link>
+              <Link className="text-secondary ">Clear All Filters</Link>
             </div>
             <hr className="text-gray-thin" />
             <div className=" flex justify-between items-center  p-4">
@@ -36,15 +37,17 @@ const SidebarFilter = () => {
             {/* //DropdownFilter // */}
 
            <DropdownFilter></DropdownFilter>
-           <hr className="text-gray-thin" />
+          
+          </div>
+         
+          
+        </div>
+        <hr className="text-gray-thin" />
            <div className="flex justify-between items-center py-4 pe-1">
               <RegularButton className="bg-transparent text-secondary ">Clear filters</RegularButton>
               <RegularButton className="py-2">Apply filters</RegularButton>
           </div>
           </div>
-         
-          
-        </div>
       </Container>
    
     </div>
