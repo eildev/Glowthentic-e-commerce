e3d112656854256ce08a11a6863e0b103616dff9
dev-kishor138 <dev.kishor138@gmail.com>
2025-01-13 15:37:42 +0600

    banner section is ongoing

--- glowthentic-frontend/src/page/home/HomePage.jsx
+++ glowthentic-frontend/src/page/home/HomePage.jsx
@@ -1,13 +1,8 @@
 
 import Container from "../../components/Container";
-import { Icon } from "@iconify/react";
-import Badge from "../../components/typography/Badge";
+
 import ScrolTop from "../../components/ScrolTop";
 import OnlineChatButton from "../../components/OnlineChatButton";
-import Toggle from "../../components/typography/Toggle";
-import Checkbox from "../../components/typography/Checkbox";
-import WishlistButton from "../../components/typography/WishlistButton";
-import IncrementDecrement from "../../components/typography/IncrementDecrement";
 import HomeSlider from "../../components/slider/HomeSlider";
 import BannerSection from "./BannerSection";
 
@@ -15,33 +10,15 @@ import BannerSection from "./BannerSection";
 const HomePage = () => {
   return (
     <div>
-        <HomeSlider></HomeSlider>
+      {/*--------- HomeSlider Section -----------*/}
+      <HomeSlider></HomeSlider>
       <Container>
-        {/* <HeadTitle>
-        <span>Discover the Latest <br />
-        Beauty Trends
-        </span>
-        </HeadTitle>
-        <RegularButton>
-        Apply filters
-        </RegularButton> */}
-        <BannerSection/>
-      
-       
-        <h2>This is Home Page</h2>
-        <div className="py-5">
-        <Badge>
-           Save 50%
-        </Badge>
-        
-        </div>
-        <Toggle></Toggle>
-        <Checkbox></Checkbox>
-        <WishlistButton></WishlistButton>
-        <IncrementDecrement/>
-        <Icon icon="solar:airbuds-remove-outline" width="24" height="24" />
+        {/*--------- Banner Section -----------*/}
+        <BannerSection />
       </Container>
+      {/*---------  OnlineChatButton -----------*/}
       <OnlineChatButton></OnlineChatButton>
+      {/*---------  ScrolTop -----------*/}
       <ScrolTop></ScrolTop>
     </div>
   );
