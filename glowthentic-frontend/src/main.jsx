52dd134fa7691c331f21909ecfea3bc00e18789e
kishormahmud9 <topfazil999@gmail.com>
2025-01-15 12:32:34 +0600

    Navbar and Appbar Design

--- glowthentic-frontend/src/main.jsx
+++ glowthentic-frontend/src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
-// import 'swiper/css';
+import "swiper/css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
