53fd1923816ad32c48ccec2748f81c688a054a74
NayemHassan <nayem15-12498@diu.edu.bd>
2025-01-22 18:03:20 +0600

    minor change

--- glowthentic-frontend/src/page/error/Page404.jsx
+++ glowthentic-frontend/src/page/error/Page404.jsx
@@ -1,10 +1,10 @@
 import HeadTitle from "../../components/typography/HeadTitle";
-
+import image from "../../assets/img/error/404.png";
 const Page404 = () => {
   return (
     <div>
       <HeadTitle>404 Not Fount</HeadTitle>
-      
+        <img src={image} alt="" />
     </div>
   );
 };
