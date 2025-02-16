caf1fb77b643ddf6971131738774dbfae846e358
omer4ruq <omerfaruq4@gmail.com>
2025-02-16 14:55:32 +0600

    floating problem solved

--- glowthentic-frontend/src/page/about/AboutBannerSection.jsx
+++ glowthentic-frontend/src/page/about/AboutBannerSection.jsx
@@ -3,7 +3,7 @@ import aboutBanner from "../../assets/img/about/Rectangle.png"
 const AboutBannerSection = () => {
     return (
         <section>
-        <div className="text-center">
+        <div className="text-center mt-12 md:mt-5">
         <h1 className="text-[#0F1228] font-bold text-[56px] leading-[68px]">Our Story</h1>
         <p className="text-lg font-normal text-[#0F1228] mx-auto max-w-[710px] mt-6 mb-4">
         Glowthentic is the collaboration hub that brings the right people, information, and tools together to get work done. From Fortune 100 companies to corner markets, millions of people around the world use Slack to connect their teams, unify their systems, and drive their business forward.
--- glowthentic-frontend/src/page/about/AboutBlog.jsx
+++ glowthentic-frontend/src/page/about/AboutBlog.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import blogImage1 from '../../assets/img/about/Blog (1).png'
 import blogImage2 from '../../assets/img/about/Blog (2).png'
 import blogImage3 from '../../assets/img/about/Blog (3).png'
+import { FaArrowRight } from "react-icons/fa";
 
 const AboutBlog = () => {
     const blogs = [
@@ -23,16 +24,20 @@ const AboutBlog = () => {
 
     ]
     return (
-        <div className='mt-32'>
-           
-            <div className='grid grid-cols-1 md:grid-cols-3 gap-8'>
+        <div className='mt-32 '>
+           <h1 className='font-bold text-3xl text-center md:text-start mb-8 md:mb-12'>Read Our Blog</h1>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-8 mx-auto'>
 {
     blogs.map((blog, index) => (
-        <div key={index} className='text-[#0F1228] text-center md:text-start'>
-            <img src={blog.image} alt="" className='rounded-md mx-auto max-w-[340px]'/>
+        <div key={index} className='text-[#0F1228] text-center md:text-start max-w-[340px] mx-auto md:mx-0'>
+            <img src={blog.image} alt="" className='rounded-md mx-auto md:mx-0 '/>
             <p className='font-bold text-sm mt-3'>{blog.subTitle}</p>
-            <h1 className='font-bold text-xl md:text-2xl mt-2 '>{blog.title}</h1>
-            <p className='text-[#FA8232] text-sm font-bold mt-6'>READ MORE</p>
+            <h1 className='font-bold text-xl md:text-2xl mt-2  '>{blog.title}</h1>
+            <div className='flex md:flex-1 gap-4 justify-center md:justify-start items-center text-[#FA8232] text-sm font-bold mt-6'>
+            <p className=''>READ MORE</p>
+            <FaArrowRight />
+            </div>
+            
         </div>
      ) )
 }
