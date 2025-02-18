82eebd66179379918994cc1b16eb9b5399dc3f26
Mdshahin12502077ms <md12502077ms@gmail.com>
2025-02-18 11:00:22 +0600

    promotion product done

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -21,3 +21,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/product', [App\Http\Controllers\Backend\ProductController::class, 'index']);
