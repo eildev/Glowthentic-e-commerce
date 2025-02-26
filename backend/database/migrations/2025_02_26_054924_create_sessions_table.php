06cecc1a2c8e2a91d0b3917173cc9441a5c080ef
omer4ruq <omerfaruq4@gmail.com>
2025-02-26 12:23:10 +0600

    delete node modules

--- backend/database/migrations/2025_02_26_054924_create_sessions_table.php
+++ backend/database/migrations/2025_02_26_054924_create_sessions_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('sessions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload'); // This is the missing column
+            $table->integer('last_activity')->index();
             $table->timestamps();
         });
     }
