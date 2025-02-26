c775a06de70390e8bf0a4001ea7d1624a384990b
omer4ruq <omerfaruq4@gmail.com>
2025-02-26 12:02:24 +0600

    jkdshfjkh

--- glowthentic-frontend/src/redux/features/api/product-api/productApi.js
+++ glowthentic-frontend/src/redux/features/api/product-api/productApi.js
@@ -2,16 +2,16 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const productApi = createApi({
     reducerPath: 'productApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://dummyjson.com' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://127.0.0.1:8000/api' }),
     endpoints: builder => ({
         getProducts: builder.query({
-            query: () => '/products?skip=0',
+            query: () => '/product?skip=0',
         }),
         searchProducts: builder.query({
-            query: (product) => `/products/search?q=${product}`,
+            query: (product) => `/product/search?q=${product}`,
         }),
         getProductByDetails: builder.query({
-            query: (id) => `/products/${id}`,
+            query: (id) => `/product/${id}`,
         }),
     })
 })
