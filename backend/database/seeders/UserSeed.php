6b213b7baa2335972e748fa959a734e30f660f49
kishormahmud9 <74523493+kishormahmud9@users.noreply.github.com>
2025-02-27 18:19:24 +0600

    hello

---
+++ backend/database/seeders/UserSeed.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'id' => 1,
+                'userName' => "Kishor",
+                'email' => 'kishor@gmail.com',
+                'password' => Hash::make('12345678'),
+                'role' => 'admin'
+            ],
+        ];
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
+    }
+}
