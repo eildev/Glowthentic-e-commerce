b8b42e6377729132cb4467555d38d7d8f5460684
omer4ruq <omerfaruq4@gmail.com>
2025-03-04 15:36:30 +0600

    product list selection state done

--- glowthentic-frontend/src/page/blog/ProductGroup.jsx
+++ glowthentic-frontend/src/page/blog/ProductGroup.jsx
@@ -1,16 +1,23 @@
-import { button, h1 } from 'framer-motion/client';
-import React from 'react';
-
+import { button, h1, text } from 'framer-motion/client';
+import React, { useState } from 'react';
+const productList = [
+    "Lipsticks", "Makeup", "Cosmetics", "Shampoo", "Hair", "LifeStyle", "Health", "Food"
+]
 const ProductGroup = () => {
-    const productList = [
-        "Lipsticks", "Makeup", "Cosmetics", "Shampoo", "Hair", "LifeStyle", "Health", "Food"
-    ]
+    const [selectedProduct, setSelectedProduct] = useState(productList[0])
+   
     return (
         <div className='border-y-[1px] border-[#FA8232] mt-20 '>
             <div className='flex gap-4  justify-between'>
                 {
                     productList.map((product) => (
-                        <button key={product}><h1  className='gap-3  py-3 text-[#B9B9B9] font-semibold text-2xl'>{product}</h1></button>
+                        <button 
+                        key={product}
+                        onClick={() => setSelectedProduct(product)}
+                        className='py-3 '
+                        ><h1  className={`gap-3  font-semibold text-2xl
+                            ${selectedProduct == product ? "text-[#FA8232]" : "text-[#B9B9B9]"}
+                            `}>{product}</h1></button>
                         
                     ))
                 }
