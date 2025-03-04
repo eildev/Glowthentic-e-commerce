7143ce049aaa2c32c41af7161f0fcf2578da91f1
omer4ruq <omerfaruq4@gmail.com>
2025-03-04 15:01:29 +0600

    main article sectopn added

--- glowthentic-frontend/src/page/blog/Blog.jsx
+++ glowthentic-frontend/src/page/blog/Blog.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import TopBanner from './TopBanner';
 import Container from '../../components/Container';
+import MainArticle from './MainArticle';
 
 const blog = () => {
     return (
         <div className=''>
             <Container>
             <TopBanner></TopBanner>
+            <MainArticle></MainArticle>
             </Container>
         </div>
     );
