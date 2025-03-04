3f61f078cfd6890641a3948be8917281f0e7fe22
omer4ruq <omerfaruq4@gmail.com>
2025-03-04 15:15:01 +0600

    main article responsive done

--- glowthentic-frontend/src/page/blog/MainArticle.jsx
+++ glowthentic-frontend/src/page/blog/MainArticle.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 const MainArticle = () => {
     return (
-        <div className='mt-20'>
-            <div className='flex gap-4 justify-center items-center font-normal text-xl text-[#646464]'>
+        <div className='nt-5 md:mt-16'>
+            <div className='flex gap-4 justify-center items-center font-normal text-xs md:text-xl text-[#646464]'>
             <p>World news </p>
             <div className='rounded-full w-2 h-2 bg-[#646464]'></div> 
             <p>12 June 2024</p>
             </div>
-           <div className='m-8'>
-            <h1 className='font-semibold text-[86px] leading-[107px] text-center text-[#0F1228]'>
+           <div className='mt-3 md:mt-8'>
+            <h1 className='font-semibold text-xl md:text-[86px] leading-6 md:leading-[107px] text-center text-[#0F1228]'>
             স্কিনকেয়ার রুটিন মানে 
 আপনার ত্বকের সাথে 
 রোজ ডেটে যাওয়া।
             </h1>
-            <p className='mt-8 text-[#0F1228] text-xl'>
+            <p className='mt-3 md:mt-8 text-[#0F1228] text-xs md:text-xl'>
             ক্লিনজিং, টোনিং, ময়েশ্চারাইজিং—এই তিনটা স্টেপ যদি ঠিকঠাক করেন, তাহলে আপনার ত্বক বলবে, "ধন্যবাদ, আমি এখন হ্যালো ম্যাগাজিনের কভার পেজে যাওয়ার জন্য প্রস্তুত!"
 ক্লিনজিং: মুখ ধোয়া মানে শুধু পানি দিয়ে মুছে ফেলা নয়। ক্লিনজার দিয়ে ভালো করে পরিষ্কার করুন, নাহলে আপনার ত্বক বলবে, "আমাকে অবহেলা করছো?"
 টোনিং: টোনার হলো সেই বন্ধু, যেটা আপনার ত্বকের pH ব্যালেন্স ঠিক রাখে। একে অবহেলা করলে ত্বক রেগে গিয়ে ব্রণ দিয়ে আক্রমণ করতে পারে!
