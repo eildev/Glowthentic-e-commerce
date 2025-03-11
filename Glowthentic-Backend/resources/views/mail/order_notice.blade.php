dab25b8dbe362036062906180828e48112c3035f
kishormahmud9 <74523493+kishormahmud9@users.noreply.github.com>
2025-03-11 09:19:50 +0600

    Delete copy backend

--- Glowthentic-Backend/resources/views/mail/order_notice.blade.php
+++
@@ -1,5 +0,0 @@
-<p>Dear Admin,<br>You have a new Order</p>
-<p>Invoice Number: {{ $New_Order['invoiceNumber'] }}.</p>
-<p>Name: {{ $New_Order['name'] }}.</p>
-<p>Phone: {{ $New_Order['phone'] }}.</p>
-<p>Check and confirm order</p>
