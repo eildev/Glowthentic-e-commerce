4fee4a54390e34a29fa13cc19b7ff08060fdc51f
dev-kishor138 <dev.kishor138@gmail.com>
2025-03-19 16:48:53 +0600

    hello world

--- glowthentic-frontend/vite.config.js
+++ glowthentic-frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // This allows access from any network interface
+    port: 5173, // Optional: Specify the port if you want to use a specific one
+  }
 })
