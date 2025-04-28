a9f4fdf44768a02501f5b2581bd8919d80280a80
Fardus Hassan <webdev.fardus@gmail.com>
2025-04-28 10:11:02 +0600

    show variant when click the cart page edit button

--- glowthentic-frontend/src/components/cart/CartItem.jsx
+++ glowthentic-frontend/src/components/cart/CartItem.jsx
@@ -56,46 +56,47 @@ const CartItem = ({ item, handleDelete }) => {
             <div className="text-sm opacity-50 mb-1">
               {item?.variant_name ?? ""}
             </div>
-            <div className="text-[#FA8232] flex items-center gap-2 cursor-pointer">
-              <svg
-                width="25"
-                height="26"
-                viewBox="0 0 25 26"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M13.859 4.63978L5.44557 13.5451C5.12789 13.8832 4.82046 14.5493 4.75898 15.0105L4.37981 18.3308C4.24659 19.5298 5.1074 20.3496 6.29614 20.1446L9.59591 19.581C10.0571 19.499 10.7027 19.1608 11.0203 18.8124L19.4338 9.90712C20.8889 8.36996 21.5448 6.61759 19.28 4.47581C17.0255 2.35453 15.3142 3.10261 13.859 4.63978Z"
-                  stroke="#FA8232"
-                  strokeWidth="1.02477"
-                  strokeMiterlimit="10"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M12.4551 6.125C12.8957 8.95338 15.1912 11.1157 18.0401 11.4026"
-                  stroke="#FA8232"
-                  strokeWidth="1.02477"
-                  strokeMiterlimit="10"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M3.34473 23.4951H21.7907"
-                  stroke="#FA8232"
-                  strokeWidth="1.02477"
-                  strokeMiterlimit="10"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-              <Link
-                to={`/product/${item?.product?.slug}?variant=${encodeURIComponent(item?.variant_name)}`}
-                className="font-medium text-base"
-              >
-                Edit
-              </Link>
-            </div>
+            <Link
+              to={`/product/${item?.product?.slug}?variant=${encodeURIComponent(item?.variant_name)}`}
+
+            >
+              <div className="text-[#FA8232] flex items-center gap-2 cursor-pointer">
+                <svg
+                  width="25"
+                  height="26"
+                  viewBox="0 0 25 26"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M13.859 4.63978L5.44557 13.5451C5.12789 13.8832 4.82046 14.5493 4.75898 15.0105L4.37981 18.3308C4.24659 19.5298 5.1074 20.3496 6.29614 20.1446L9.59591 19.581C10.0571 19.499 10.7027 19.1608 11.0203 18.8124L19.4338 9.90712C20.8889 8.36996 21.5448 6.61759 19.28 4.47581C17.0255 2.35453 15.3142 3.10261 13.859 4.63978Z"
+                    stroke="#FA8232"
+                    strokeWidth="1.02477"
+                    strokeMiterlimit="10"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M12.4551 6.125C12.8957 8.95338 15.1912 11.1157 18.0401 11.4026"
+                    stroke="#FA8232"
+                    strokeWidth="1.02477"
+                    strokeMiterlimit="10"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M3.34473 23.4951H21.7907"
+                    stroke="#FA8232"
+                    strokeWidth="1.02477"
+                    strokeMiterlimit="10"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+
+                <span>Edit</span>
+              </div>
+            </Link>
           </div>
         </div>
       </td>
