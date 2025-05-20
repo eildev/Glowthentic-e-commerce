a5255933d8bc625d9f8f04c6afde5c13033a38e8
kishormahmud9 <74523493+kishormahmud9@users.noreply.github.com>
2025-05-20 11:05:13 +0600

    change url

--- glowthentic-frontend/src/redux/features/api/registerApi/registerApi.js
+++ glowthentic-frontend/src/redux/features/api/registerApi/registerApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const registerApi = createApi({
   reducerPath: 'registerApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://backend.glowthentic.store/api' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://127.0.0.1:8000/api' }),
   endpoints: (builder) => ({
     registerInfo: builder.mutation({
       query: (registerData) => ({
