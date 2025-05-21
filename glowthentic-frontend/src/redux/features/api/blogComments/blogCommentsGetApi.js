70d4bc7580031bf3a11d0f054a2f0c0ac98027f3
kishormahmud9 <74523493+kishormahmud9@users.noreply.github.com>
2025-05-21 16:20:56 +0600

    change url

--- glowthentic-frontend/src/redux/features/api/blogComments/blogCommentsGetApi.js
+++ glowthentic-frontend/src/redux/features/api/blogComments/blogCommentsGetApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const blogCommentsGetApi = createApi({
   reducerPath: 'blogCommentsGetApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://backend.glowthentic.store/api',
+    baseUrl: 'http://127.0.0.1:8000/api',
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
       if (token) {
